3bde820d8de6dd417bf5eea73bf7f38b9cdbc86e
Stephen Smith <stephensmith_aus@yahoo.com.au>
2024-12-30 07:47:03 +1100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,10 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options => 
   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+  opt
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("https://localhost:3000");
+});
+
 app.MapControllers();
 DbInitializer.InitDb(app);
 app.Run();
---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,16 @@
+import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
+
+type Props = {
+  products: Product[];
+}
+
+const Catalog = ({products}: Props) => {
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
+};
+
+export default Catalog;
